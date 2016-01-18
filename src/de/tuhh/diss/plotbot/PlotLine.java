2535e14e4e3ce56e8590d6da45766593d2a5e3e3
chris <c.kahlefendt@gmail.com>
2016-01-18 20:08:57 +0100

    changed PlotLine class

--- src/de/tuhh/diss/plotbot/PlotLine.java
+++ src/de/tuhh/diss/plotbot/PlotLine.java
@@ -1,35 +1,14 @@
 package de.tuhh.diss.plotbot;
 
-import de.tuhh.diss.exceptions.MotorException;
-import de.tuhh.diss.exceptions.OutOfWorkspaceException;
 import de.tuhh.diss.robot.RobotInterface;
 
 public abstract class PlotLine {
-	
-	private static final int speed = 450;
-	
+
 	protected RobotInterface robot;
 
-	public PlotLine(RobotInterface robot, int xStart, int yStart) throws MotorException{
+	public PlotLine(RobotInterface robot, int xStart, int yStart){
 		
 		this.robot = robot; 
-		moveToCoord(xStart, yStart);
-	}
-
-	protected void moveToCoord(int x, int y) throws MotorException{
-		
-		robot.setWheelSpeed(speed);
-		robot.setArmSpeed(speed);
-		
-		try {
-			robot.moveArmTo(CoordTrans.getAngle(robot.getArmLength(), x), true);
-			robot.moveWheels(CoordTrans.getFeed(robot.getArmLength(), robot.getMaxFeed(), x, y) - CoordTrans.getYPosition(robot.getArmLength(), robot.getFeed(), robot.getArmAngle()));
-		} catch (OutOfWorkspaceException e) {
-			robot.stopAllMotors();
-			throw new MotorException();
-		}
-		
-				
-		robot.waitForArm();
+		robot.movePenTo(xStart, yStart);
 	}
 }
