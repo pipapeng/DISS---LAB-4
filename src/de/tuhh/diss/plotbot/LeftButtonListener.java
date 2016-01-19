f0e62709ffaa4f143843ad13c61cf59b0516fc2f
pipapeng <lennart-heeren@t-online.de>
2016-01-19 20:38:51 +0100

    keine ahnung

---
+++ src/de/tuhh/diss/plotbot/LeftButtonListener.java
@@ -0,0 +1,15 @@
+package de.tuhh.diss.plotbot;
+
+import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
+
+public class LeftButtonListener implements ButtonListener{
+	
+	public void buttonPressed(Button b){
+		
+	}
+	
+	public void buttonReleased(Button b){
+		
+	}
+}
--- src/de/tuhh/diss/plotbot/Plotbot.java
+++ src/de/tuhh/diss/plotbot/Plotbot.java
@@ -5,74 +5,9 @@ import de.tuhh.diss.robot.PhysicalRobot;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 
-public class Plotbot {
+public class Plotbot{
 	
-	private static int curserPosition;
-	private static int choice;
-	
-	
-	public static void main(String[] args){
 		
-		choice = 0;
-		curserPosition = 0;
-		
-		LCD.drawString("Welcome to", 0, 0);
-		LCD.drawString("Plotbot!", 0, 1);
-	
-		do{
-			
-			mainMenu();
-			
-			
-			
-			
-			Button.ENTER.waitForPressAndRelease();
-			choice = curserPosition;
-			switch(choice){
-			
-			case 1:
-				
-			case 2:
-				
-			}
-			
-		}while(choice != 3);
-		
-		LCD.drawString("Plotbot!", 0, 1);
-		Button.ESCAPE.waitForPressAndRelease(); //wait a sec
-	}
-	
-	
-	
-	private static void incrementCurserPosition(){
-		curserPosition++;
-		if(curserPosition > 3){
-			curserPosition = 1;
-		}
-	}
-	
-	private static void decrementCurserPosition(){
-		curserPosition--;
-		if(curserPosition < 1){
-			curserPosition = 3;
-		}
-	}
-	
-	private static void mainMenu(){
-		LCD.clear();
-		LCD.drawString("*** Main menu ***", 0, 0);
-		LCD.drawString("Select shape!", 0, 3);
-		
-		LCD.drawString("Rectangle", 3, 4);
-		LCD.drawString("String", 3, 5);
-		LCD.drawString("Quit Programm", 3, 6);
-		
-		if(curserPosition <= 3 && curserPosition >= 1){
-			
-			LCD.drawString("X", 0, curserPosition + 3);
-		}
-	}	
-	
 	
 	
 }
