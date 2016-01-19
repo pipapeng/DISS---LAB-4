fdc55ee1e912f9b0353c83c991c02cc418644457
pipapeng <lennart-heeren@t-online.de>
2016-01-19 21:19:43 +0100

    PlotterInterface

--- src/de/tuhh/diss/plotter/PlotterInterface.java
+++ src/de/tuhh/diss/plotter/PlotterInterface.java
@@ -2,4 +2,11 @@ package de.tuhh.diss.plotter;
 
 public interface PlotterInterface {
 
+	// Wuerde vorschlagen Robot in Plotter zu erzeugen (Lennart)
+	
+	public void plotRectangle(double size);
+	public void plotString(double size);
+	
+	public void stopImmediatly();
+	public void shutDown();				// brauch man das?
 }
