855dd9a8b59dc5e8e60d821f9c7ed6883ae0449e
pipapeng <lennart-heeren@t-online.de>
2016-01-19 21:29:46 +0100

    Plotter benoetigte Methoden gebaut (leer)

--- src/de/tuhh/diss/plotbot/Plotbot.java
+++ src/de/tuhh/diss/plotbot/Plotbot.java
@@ -1,12 +1,20 @@
 package de.tuhh.diss.plotbot;
 
-import de.tuhh.diss.robot.RobotInterface;
-import de.tuhh.diss.robot.PhysicalRobot;
+import de.tuhh.diss.plotter.Plotter;
+import de.tuhh.diss.plotter.PlotterInterface;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 
 public class Plotbot{
 	
+	public static void main(String[] args)
+	{
+		PlotterInterface plotter = new Plotter();
+		
+
+
+		//Button.ESCAPE.waitForPressAndRelease();
+	}
 		
 	
 	
--- src/de/tuhh/diss/plotter/Plotter.java
+++ src/de/tuhh/diss/plotter/Plotter.java
@@ -1,5 +1,32 @@
 package de.tuhh.diss.plotter;
 
-public class Plotter {
+public class Plotter implements PlotterInterface{
 
+	public Plotter(){
+		
+	}
+
+	@Override
+	public void plotRectangle(double size) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void plotString(double size) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopImmediatly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shutDown() {
+		// TODO Auto-generated method stub
+		
+	}
 }
