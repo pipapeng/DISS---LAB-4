9f80e599d6cdf1977050a0d95953f422910250e3
pipapeng <lennart-heeren@t-online.de>
2016-01-20 10:20:09 +0100

    Bugfix: size- und mainmenu (Listener ENTER gebaut)

--- src/de/tuhh/diss/plotbot/UserInterface.java
+++ src/de/tuhh/diss/plotbot/UserInterface.java
@@ -13,14 +13,10 @@ public class UserInterface implements ButtonListener{
 	private int maxSize;
 	private int size;
 	
-	private boolean mainMenu;
-	private boolean sizeMenu;
+	private boolean mainMenuActive;
+	private boolean sizeMenuActive;
 	
 	public UserInterface(){
-		
-		curserPosition = 1;
-		repeatsLeft = 0;
-		repeatsRight = 0;
 		
 		LCD.drawString("Welcome to", 3, 1);
 		LCD.drawString("Plotbot!", 4, 2);
@@ -30,7 +26,8 @@ public class UserInterface implements ButtonListener{
 	
 	public int mainMenu() {
 		
-		mainMenu = true;
+		mainMenuActive = true;
+		curserPosition = 1;
 		
 		LCD.clear();
 		LCD.drawString("** Main menu **", 0, 0);
@@ -40,44 +37,53 @@ public class UserInterface implements ButtonListener{
 		LCD.drawString("String", 3, 5);
 		LCD.drawString("Quit Program", 3, 6);
 		
-		LCD.drawString("X", 0, curserPosition + 3);
-		
-		Button.LEFT.addButtonListener(this);
-		Button.RIGHT.addButtonListener(this);
-		
-		
-		Button.ENTER.waitForPressAndRelease();
-		mainMenu = false;
+		do{
+			LCD.clear(0, curserPosition + 2, 1);
+			LCD.drawString("X", 0, curserPosition + 3);
+			
+			Button.LEFT.addButtonListener(this);
+			Button.RIGHT.addButtonListener(this);
+			Button.ENTER.addButtonListener(this);
+		} while(mainMenuActive == true);
+	
 		return curserPosition;
 	}
 	
 	public int sizeMenu(int minSize, int maxSize){
 		
+		repeatsLeft = 0;
+		repeatsRight = 0;
+		
 		this.minSize = minSize;
 		this.maxSize = maxSize;
 		
-		if(sizeMenu == false){
+		if(sizeMenuActive == false){
 			size = (int) ((minSize + maxSize) / 20) * 10;
 		}
 		
-		sizeMenu = true;
+		sizeMenuActive = true;
 		
 		LCD.clear();
 		LCD.drawString("** Size menu **", 0, 0);
-		LCD.drawString("Max Size:", 0, 2);
-		LCD.drawString(Integer.toString(maxSize), 0, 3);
+		
+		LCD.drawString("Min Size:", 0, 2);
+		LCD.drawString(Integer.toString(minSize), 10, 2);
+		LCD.drawString("Max Size:", 0, 3);
+		LCD.drawString(Integer.toString(maxSize), 10, 3);
 		
 		LCD.drawString("Size:", 0, 5);
-		LCD.drawString(Integer.toString(size), 7, 5);
 		
-		// nochmal oder reicht das einmal?
-		Button.LEFT.addButtonListener(this);
-		Button.RIGHT.addButtonListener(this);
-		Button.ESCAPE.addButtonListener(this);
-		//TODO: ESCAPE -> return to main menu
+		do{
+			LCD.clear(5);
+			LCD.drawString(Integer.toString(size), 7, 5);
+			
+			// nochmal oder reicht das einmal?
+			Button.LEFT.addButtonListener(this);
+			Button.RIGHT.addButtonListener(this);
+			Button.ESCAPE.addButtonListener(this);
+			//TODO: ESCAPE -> return to main menu
+		} while(sizeMenuActive == true);
 		
-		Button.ENTER.waitForPressAndRelease();
-		sizeMenu = false;
 		return size;
 	}
 	
@@ -128,36 +134,46 @@ public class UserInterface implements ButtonListener{
 
 		if(b.getId() == Button.ID_RIGHT){
 			
-			if(mainMenu == true){
+			if(mainMenuActive == true){
 				
 				incrementCurserPosition();
 			}
-			if(sizeMenu == true){
+			if(sizeMenuActive == true){
 				
 				incrementSize();
-				sizeMenu(minSize, maxSize);
 			}
 		}
 		
 		if(b.getId() == Button.ID_LEFT){
 			
-			if(mainMenu == true){
+			if(mainMenuActive == true){
 				
 				decrementCurserPosition();
 			}
-			if(sizeMenu == true){
+			if(sizeMenuActive == true){
 				
 				decrementSize();
-				sizeMenu(minSize, maxSize);
+			}
+		}
+		
+		if(b.getId() == Button.ID_ENTER){
+		
+			if(mainMenuActive == true){
+				
+				mainMenuActive = false;
+			}
+			if(sizeMenuActive == true){
+				
+				sizeMenuActive = false;
 			}
 		}
 		
 		if(b.getId() == Button.ID_ESCAPE){
 			
-			if(sizeMenu == true){
+			if(sizeMenuActive == true){
 				
-				sizeMenu = false;
-				// raus aus size und mainMenu() aufrufen
+				sizeMenuActive = false;
+				//TODO; raus aus size und mainMenu() aufrufen
 			}
 		}
 	}
