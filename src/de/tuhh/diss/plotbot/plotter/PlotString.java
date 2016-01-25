58668cbaf74c5afa4c0e60d31073028c5237fe3b
Julius <Juliusharms@gmx.de>
2016-01-25 16:42:24 +0100

    Plot Rectangle und String verbessert.

--- src/de/tuhh/diss/plotbot/plotter/PlotString.java
+++ src/de/tuhh/diss/plotbot/plotter/PlotString.java
@@ -5,97 +5,88 @@ import de.tuhh.diss.plotbot.robot.RobotInterface;
 
 public class PlotString {
 
-	public RobotInterface robot;
-	private static final double scale = 1/6;
-	private static final int start = 230;
-	private static final double center = 1/2;
+	private static final double SCALE = 1/6;
+	private static final int START = 230;
+	private static final double CENTER = 1/2;
+	private RobotInterface robot;
+	private static final int MINSIZE = 10;
+	private static final int MAXSIZE = (int) (START*(26*SCALE));
 	
-	public double PlotString(int size){
-	robot = PlotRectangle.getRobot();
-	double nextY;
+	public PlotString(RobotInterface robot){
+		this.robot = robot;
+	}
 	
-	nextY = getSpacingFrameToString(size, start);
-	nextY = drawT(size, nextY);
-	nextY = getSpacingStringToString(size, nextY);
-	nextY = drawU(size, nextY);
-	nextY = getSpacingStringToString(size, nextY);
-	nextY = drawH(size, nextY);
-	nextY = getSpacingStringToString(size, nextY);
-	nextY = drawH(size, nextY);
-	nextY = getSpacingFrameToString(size, nextY);
+	public int getMinSize(){
+		return MINSIZE;
+	}
 	
-	return nextY;
+	public int getMaxSize(){
+		return MAXSIZE;
+	}
+	
+	public double drawTUHH(int size){
+		double nextY;
+		
+		nextY = getSpacingFrameToString(size, START);
+		nextY = drawT(size, nextY);
+		nextY = getSpacingStringToString(size, nextY);
+		nextY = drawU(size, nextY);
+		nextY = getSpacingStringToString(size, nextY);
+		nextY = drawH(size, nextY);
+		nextY = getSpacingStringToString(size, nextY);
+		nextY = drawH(size, nextY);
+		nextY = getSpacingFrameToString(size, nextY);
+		
+		
+		return nextY;
 	}
 	
 	private double getSpacingFrameToString(int size, double startY){
-		return startY-2*scale*size;
+		return startY-2*SCALE*size;
 	}
 	
 	private double getSpacingStringToString(int size, double startY){
-		return startY-3*scale*size;
+		return startY-3*SCALE*size;
 	}
 	
 	private double drawT(int size, double startY){
-	double widthY = 4*scale*size;
-	double endY = startY-widthY;
+		double widthY = 4*SCALE*size;
+		double endY = startY-widthY;
 	
 		try {
-		new PlotVerticalLine(robot, center*size, startY, -widthY);
-	} catch (MotorException e) {
-		e.printStackTrace();
-	}
-	try {
-		new PlotHorizontalLine(robot, center*size, startY-widthY*center, -size);
-	} catch (MotorException e) {
-		e.printStackTrace();
-	}
-	
-	return endY;
+			new PlotVerticalLine(robot, CENTER*size, startY, -widthY);
+			new PlotHorizontalLine(robot, CENTER*size, startY-widthY*CENTER, -size);
+		} catch (MotorException e) {
+			e.printStackTrace();
+		}
+			return endY;
 	}
 	
 	private double drawU(int size, double startY){
-		double widthY = 3*scale*size;
+		double widthY = 3*SCALE*size;
 		double endY = startY-widthY;
 		
 		try {
-			new PlotHorizontalLine(robot, center*size, startY, -size);
+			new PlotHorizontalLine(robot, CENTER*size, startY, -size);
+			new PlotVerticalLine(robot, -CENTER*size, startY, -widthY);
+			new PlotHorizontalLine(robot, -CENTER*size, startY-widthY, size);
 		} catch (MotorException e) {
 			e.printStackTrace();
 		}
-		try {
-			new PlotVerticalLine(robot, -center*size, startY, -widthY);
-		} catch (MotorException e) {
-			e.printStackTrace();
-		}
-		try {
-			new PlotHorizontalLine(robot, -center*size, startY-widthY, size);
-		} catch (MotorException e) {
-			e.printStackTrace();
-		}
-		
 		return endY;
 		}
 	
 	private double drawH(int size, double startY){
-		double widthY = 3*scale*size;
+		double widthY = 3*SCALE*size;
 		double endY = startY-widthY;
 		
 		try {
-			new PlotHorizontalLine(robot,  center*size, startY, -size);
-		} catch (MotorException e) {
-			e.printStackTrace();
-		}
-		try {
+			new PlotHorizontalLine(robot,  CENTER*size, startY, -size);
 			new PlotVerticalLine(robot, 0, startY, widthY);
+			new PlotHorizontalLine(robot, -CENTER*size, startY-widthY, size);
 		} catch (MotorException e) {
 			e.printStackTrace();
 		}
-		try {
-			new PlotHorizontalLine(robot, -center*size, startY-widthY, size);
-		} catch (MotorException e) {
-			e.printStackTrace();
-		}
-		
 		return endY;
 		}	
 	
